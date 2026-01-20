05b0a67428d0fce72cddd2698fb4e336f419f926
Will123iam <98259777+Will123iam@users.noreply.github.com>
2026-01-20 13:34:03 +0000

    Multi windows support / can now load into next file

--- canvas.py
+++ canvas.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 from tkinter import ttk
-from classes import file_widget, style_creation, frame_creation
+from classes import file_widget, style_creation, frame_creation, scrollable_frame
 from repeat_functions import rows_colums
 import os
 
@@ -15,6 +15,19 @@ win_canvas.geometry("1200x800")
 win_canvas.title(f"File Manager - {path}")
 win_canvas.config(bg="gray")
 rows_colums(win_canvas,0,2)
+
+def update_winsize(side_select_canvas,view_container): #Updates sizing / placemnts on screen
+    #global win_size_hight
+    win_canvas.update_idletasks()
+    win_size_hight = win_canvas.winfo_height()
+    win_size_width = win_canvas.winfo_width()
+    #print("y=",win_size_hight,"x=",win_size_width)
+    #Updates widgets
+    side_select_canvas.configure(height=(win_size_hight-7))
+    view_container.configure(width=(win_size_width-240))
+
+    win_canvas.after(500,lambda: update_winsize(side_select_canvas,view_container))
+
 
 #Load file icon
 file_icon=tk.PhotoImage(file="images/icon_file.png")
@@ -31,7 +44,7 @@ for file in files: count+=1
 side_selection_container = ttk.Frame(win_canvas)
 side_selection_container.grid(row=0,column=0,sticky='w')
 
-side_select_canvas = tk.Canvas(side_selection_container,width=220,height=800)
+side_select_canvas = tk.Canvas(side_selection_container,bg="dark turquoise",width=220,height=793)
 side_select_canvas.pack(side="left",fill="both",expand=True)
 
 scrollbar = ttk.Scrollbar(side_selection_container,orient="vertical",command=side_select_canvas.yview)
@@ -42,15 +55,24 @@ side_select_canvas.configure(yscrollcommand=scrollbar.set)
 scroll_frame = frame_creation(side_select_canvas,int(count/2),2,style="turquoise.TFrame")
 side_select_canvas.create_window((0,0),window=scroll_frame,anchor='nw')
 
+#Finder like windows
+view_container=frame_creation(win_canvas,2,2,width=950)
+view_container.grid(row=0,column=1,sticky="nsew")
+view_container.grid_propagate(False)
+
+
 #Loads files into selection menu
-x,y=0,0
-for file in files:
-    icon=file_widget(scroll_frame,file,file_icon,"blue.TFrame","blue.TLabel")
-    icon.place(y,x)
-    if x == 1: 
-        x=0
-        y+=1
-    else: x+= 1
-        
+def disply_files(files):
+    x,y=0,0
+    for file in files:
+        icon=file_widget(scroll_frame,file,file_icon,"blue.TFrame","blue.TLabel",view_container,path)
+        icon.place(y,x)
+        if x == 1: 
+            x=0
+            y+=1
+        else: x+= 1
+
+disply_files(files)
+update_winsize(side_select_canvas,view_container)
 
 win_canvas.mainloop()
--- classes.py
+++ classes.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import ttk
+import os
 
 class frame_creation(ttk.Frame):
     def __init__(self,window,rows,columns,**kwargs):
@@ -16,9 +17,13 @@ class style_creation(ttk.Style):
         for item in items: self.configure(f"{name}.T{item}", background=bg)
 
 class file_widget():
-    def __init__(self,window,file,icon_image,style1,style2):
+    def __init__(self,window,file,icon_image,style1,style2,view_container,previous_path):
 
+        self.view_container=view_container
         self.file=file
+        self.detection_valuex=-1
+        self.previous_path = previous_path
+
         if len(file) > 15: 
             file=file[:15] #Sets text size limit
             file+="..."
@@ -35,9 +40,69 @@ class file_widget():
         self.file_icon.bind("<Button-1>",self.when_clicked)
         self.file_name.bind("<Button-1>",self.when_clicked)
 
+    def update_loop(self):
+        self.view_container.update_idletasks()
+        win_size_hight = self.view_container.winfo_height()
+        win_size_width = self.view_container.winfo_width()
+
+        self.view_frame.configure(width=win_size_width-100,height=win_size_hight-400)
+
+        self.view_container.after(500,self.update_loop)
+
+
     def place(self,row,column):
         self.icon_fram.grid(row=row,column=column,padx=3,pady=3)
 
     def when_clicked(self,event):
-        print(self.file,"was pressed!")
+        self.detection()
+        self.view_frame=scrollable_frame(self.view_container,"dark turquoise",450,350,"turquoise.TFrame",0,self.detection_valuex,self.view_container,(self.previous_path+"/"+self.file))
+        self.inner_files=self.view_frame.load_content(self.previous_path,self.file)
+        self.view_frame.disply_file(self.inner_files)
 
+    def detection(self):
+        self.detection_valuex += 1
+        if self.detection_valuex > 3: self.detection_valuex = 0
+        
+
+
+class scrollable_frame(ttk.Frame):
+    def __init__(self,container_frame,bg,width,hight,style,row,column,view_container,pv_path):
+        ttk.Frame.__init__(self,container_frame,style=style,relief="ridge")
+        self.grid(row=row,column=column,sticky="nw",padx=10,pady=10)
+        self.grid_propagate(False)
+        self.file_icon=tk.PhotoImage(file="images/icon_file.png")
+        self.view_container=view_container
+        self.pv_path=pv_path
+
+        canvas =tk.Canvas(self,bg=bg,width=width,height=hight)
+        canvas.pack(side="left",fill="both",expand=True,padx=5,pady=5)
+
+        scrollbar=ttk.Scrollbar(self,orient="vertical",command=canvas.yview)
+        scrollbar.pack(side='right',fill='y')
+
+        self.scroll_frame=frame_creation(canvas,3,3,style=style)
+        canvas.create_window((0,0),window=self.scroll_frame,anchor='nw')
+
+    def load_content(self,pv_path,file):
+        new_file=""
+        for char in file:
+            if char == " ":
+                char="/"
+            new_file+=char
+
+        #path=open("path.txt",'r')
+        path=pv_path+"/"+file
+        files=os.listdir(path)
+        print("Loaded!")
+
+        return files
+    
+    def disply_file(self,files):
+        x,y=0,0
+        for file in files:
+            icon=file_widget(self.scroll_frame,file,self.file_icon,"blue.TFrame","blue.TLabel",self.view_container,self.pv_path)
+            icon.place(y,x)
+            if x == 4: 
+                x=0
+                y+=1
+            else: x+= 1
