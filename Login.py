be2ebbb7ff1bfff0f4f94e4612dd642d4585835f
Will123iam <98259777+Will123iam@users.noreply.github.com>
2026-01-21 07:54:49 +0000

    saves entered path for next opening and as a path label in the bottom left

--- Login.py
+++ Login.py
@@ -1,7 +1,9 @@
 import tkinter as tk
-from tkinter import ttk
-from classes import *#frame_creation, style_creation
-from repeat_functions import *#rows_colums, error_message
+from tkinter import ttk, font
+from classes import *
+from repeat_functions import *
+
+#Section one
 
 def pathsave(file,item):
     data=open(file,'w')
@@ -10,9 +12,13 @@ def pathsave(file,item):
 
 def load_canvas(): #Checks if input given, moves onto next screen
     if device_input.get():
-        login_win.destroy()
-        pathsave("path.txt",device_input.get())
-        #exec(open("canvas.py").read()) #Opens / runs main app
+        try:
+            os.listdir(device_input.get())
+            login_win.destroy()
+            pathsave("path.txt",device_input.get())
+        except:
+            error_message(login_win,"Location not found!",2,1)
+
     else: error_message(login_win,"No location given!",2,1)
 
 #Main window
@@ -23,9 +29,11 @@ login_win.title("File Manager - Login")
 login_win.config(bg="lemon chiffon")
 rows_colums(login_win,3,3) #Adds a grid
 
-white_style=style_creation("white","classic","white",items=["Label"]) #White style
-device_input=tk.StringVar(value=None) #Input varible for where to look for loading files
+old_path=open("path.txt",'r')
+old_path=old_path.readline()
 
+white_style=style_creation("white","classic","white",items=["Label","Frame"]) #White style
+device_input=tk.StringVar(value=old_path) #Input varible for where to look for loading files
 
 #File / Device location input
 connection_frame = frame_creation(login_win,2,2,relief="groove")
@@ -41,12 +49,12 @@ continue_butt=ttk.Button(connection_frame,text="Ok",command=load_canvas)
 continue_butt.grid(row=1,column=1,padx=5,pady=5)
 #Input End
 
-
 login_win.mainloop()
 
+#Section two
+
 #Loads path
-path=open("path.txt",'r')
-path=path.readline()
+path=device_input.get()
 files=os.listdir(path)
 
 #Main window
@@ -56,14 +64,25 @@ win_canvas.title(f"File Manager - {path}")
 win_canvas.config(bg="gray")
 rows_colums(win_canvas,0,2)
 
+#Loads files into selection menu
+def disply_files(files):
+    x,y=0,0
+    for file in files:
+        icon=file_widget(scroll_frame,file,file_icon,"blue.TFrame","blue.TLabel",view_container,path)
+        icon.place(y,x)
+        if x == 1: 
+            x=0
+            y+=1
+        else: x+= 1
+
 def update_winsize(side_select_canvas,view_container): #Updates sizing / placemnts on screen
     #global win_size_hight
     win_canvas.update_idletasks()
     win_size_hight = win_canvas.winfo_height()
     win_size_width = win_canvas.winfo_width()
-    #print("y=",win_size_hight,"x=",win_size_width)
+
     #Updates widgets
-    side_select_canvas.configure(height=(win_size_hight-7))
+    side_select_canvas.configure(height=(win_size_hight-35))
     view_container.configure(width=(win_size_width-240))
 
     win_canvas.after(500,lambda: update_winsize(side_select_canvas,view_container))
@@ -81,36 +100,56 @@ count=0
 for file in files: count+=1
 
 #File selection
-side_selection_container = ttk.Frame(win_canvas)
+side_selection_container = frame_creation(win_canvas,2,2)
 side_selection_container.grid(row=0,column=0,sticky='w')
 
 side_select_canvas = tk.Canvas(side_selection_container,bg="dark turquoise",width=220,height=793)
-side_select_canvas.pack(side="left",fill="both",expand=True)
+side_selection_container.grid_columnconfigure(0,weight=3)
+side_select_canvas.grid(row=0,column=0,sticky="nesw")
+#side_select_canvas.pack(side="left",fill="both",expand=True)
 
 scrollbar = ttk.Scrollbar(side_selection_container,orient="vertical",command=side_select_canvas.yview)
-scrollbar.pack(side='right',fill='y')
+scrollbar.grid(row=0,column=1,sticky='nes')
+#scrollbar.pack(side='right',fill='y')
 
 side_select_canvas.configure(yscrollcommand=scrollbar.set)
 
 scroll_frame = frame_creation(side_select_canvas,int(count/2),2,style="turquoise.TFrame")
 side_select_canvas.create_window((0,0),window=scroll_frame,anchor='nw')
 
+def shrink(event):
+    label = event
+    print(label)
+
+    f = font.nametofont("pathFont")
+    text = label.cget("text")
+    len=235
+
+    for size in range(20,5,-1):
+        f.configure(size=size)
+        if f.measure(text) <= len:
+            print(f.measure(text),":",len)
+            break
+
+pathFont=font.Font(family="Helvetica",size=15,name="pathFont")
+
+#Show path
+path_frame=frame_creation(side_selection_container,1,1,style="white.TFrame")
+path_frame.grid(row=1,column=0,sticky='wes')
+path_frame.grid_propagate(False)
+
+path_label=ttk.Label(path_frame,text=path,font="pathFont",style="white.TLabel")
+path_label.pack(side='left',pady=3)
+shrink(path_label)
+
+#ttk.Label(side_selection_container,text="rtj5e6je56j").grid(row=1,column=1)
+
+#path_label.bind("<Configure>",shrink)
+
 #Finder like windows
 view_container=frame_creation(win_canvas,2,2,width=950)
 view_container.grid(row=0,column=1,sticky="nsew")
 view_container.grid_propagate(False)
-
-
-#Loads files into selection menu
-def disply_files(files):
-    x,y=0,0
-    for file in files:
-        icon=file_widget(scroll_frame,file,file_icon,"blue.TFrame","blue.TLabel",view_container,path)
-        icon.place(y,x)
-        if x == 1: 
-            x=0
-            y+=1
-        else: x+= 1
 
 disply_files(files)
 update_winsize(side_select_canvas,view_container)
